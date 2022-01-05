7fd300ae72c39d45c07d8c48599c039ccb212e11
brandon.jeong-nsuslab <brandon.jeong@nsuslab.com>
2022-01-05 20:00:42 +0900

    4th commit

--- src/layouts/components/vertical-nav-menu/VerticalNavMenu.vue
+++ src/layouts/components/vertical-nav-menu/VerticalNavMenu.vue
@@ -11,7 +11,7 @@
     <!-- Navigation Header -->
     <div class="vertical-nav-header d-flex items-center ps-6 pe-5 pt-5 pb-2">
       <router-link to="/" class="d-flex align-center text-decoration-none">
-        <v-img
+        <!-- <v-img
           :src="require('@/assets/images/logos/logo.svg')"
           max-height="30px"
           max-width="30px"
@@ -19,7 +19,7 @@
           contain
           eager
           class="app-logo me-3"
-        ></v-img>
+        ></v-img> -->
         <v-slide-x-transition>
           <h2 class="app-title text--primary">TABLE GAMES</h2>
         </v-slide-x-transition>
